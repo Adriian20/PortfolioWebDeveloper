5e900e88fe3a6ef905076ecb71164990d6256457
Adriian20 <adrianmarquesroqueta@gmail.com>
2025-01-06 20:53:08 +0100

    Portfolio

--- js/script.js
+++ js/script.js
@@ -1,21 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-    /* const header = document.querySelector('header');
-    let lastScrollTop = 0;
-    window.addEventListener('scroll', function () {
-        const scrollTop = window.scrollY;
-        if (scrollTop > lastScrollTop) {
-            header.style.transform = 'translateY(-100%)';
-        } else {
-            header.style.transform = 'translateY(0)';
-        }
-        lastScrollTop = scrollTop;
-    });
-    header.style.position = 'fixed';
-    header.style.top = '0';
-    header.style.width = '100%';
-    header.style.transition = 'transform 0.3s ease';
-    */
-
     // Animation links
     const links = document.querySelectorAll('a');
     links.forEach(link => {
